9031c45bcf9e9b7773ad0304b14b73eef2607cc6
pauloliriopereira <pauloliriopereira@gmail.com>
2017-01-09 15:10:11 -0200

    alteração

--- src/exercicio_5_16/GraficoDeAsteriscos.java
+++ src/exercicio_5_16/GraficoDeAsteriscos.java
@@ -77,6 +77,7 @@ public class GraficoDeAsteriscos
 	
 	public void imprGrafAsterisco( int numero )
 	{
+		System.out.println();
 		for( int i = 0; i < numero; i++ ) 
 		{
 			System.out.print( '*' );
