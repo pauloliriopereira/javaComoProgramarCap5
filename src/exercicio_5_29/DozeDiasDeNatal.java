6561b9307a5732ac133a289ddeb9ec6407f5fa44
pauloliriopereira <pauloliriopereira@gmail.com>
2017-01-16 17:08:16 -0200

    exercício 5.29

---
+++ src/exercicio_5_29/DozeDiasDeNatal.java
@@ -0,0 +1,159 @@
+package exercicio_5_29;
+
+public class DozeDiasDeNatal 
+{
+	public static void main( String[] args ) 
+	{
+		for( int i = 1; i <= 12; i++ ) 
+		{
+			switch( i )
+			{
+				case 1 : System.out.println( "first " );
+				break;
+				case 2 : System.out.println( "second" );
+				break;
+				case 3 : System.out.println( "third" );
+				break;
+				case 4 : System.out.println( "fourth" );
+				break;
+				case 5 : System.out.println( "fifth" );
+				break;
+				case 6 : System.out.println( "sixth" );
+				break;
+				case 7 : System.out.println( "seventh" );
+				break;
+				case 8 : System.out.println( "eighth" );
+				break;
+				case 9 : System.out.println( "ninth" );
+				break;
+				case 10 : System.out.println( "tenth" );
+				break;
+				case 11 : System.out.println( "eleventh " );
+				break;
+				case 12 : System.out.println( "twelfth" );
+				break;
+			}
+			switch( i )
+			{
+				case 1 : 
+					System.out.println( "On the first day of Christmas,\n"
+										+ "my true love sent to me\n"
+										+ "A partridge in a pear tree.\n" );
+				break;
+				case 2 : 
+					System.out.println( "On the second day of Christmas,\n"
+										+ "my true love sent to me\n"
+										+ "Two turtle doves,\n"
+										+ "And a partridge in a pear tree.\n" );
+				break;
+				case 3 : System.out.println( "On the third day of Christmas,\n"
+											 + "my true love sent to me\n"
+											 + "Three French hens,\n"
+											 + "Two turtle doves,\n"
+											 + "And a partridge in a pear tree.\n" );
+				break;
+				case 4 : System.out.println( "On the fourth day of Christmas,\n"
+											 + "my true love sent to me\n"
+											 + "Four calling birds,\n"
+											 + "Three French hens,\n"
+											 + "Two turtle doves,\n"
+											 + "And a partridge in a pear tree.\n" );
+				break;
+				case 5 : System.out.println( "On the fifth day of Christmas,\n"
+											 + "my true love sent to me\n"
+											 + "Five golden rings,\n"
+											 + "Four calling birds,\n"
+											 + "Three French hens,\n"
+											 + "Two turtle doves,\n"
+											 + "And a partridge in a pear tree.\n" );
+				break;
+				case 6 : System.out.println( "On the sixth day of Christmas,\n"
+											 + "my true love sent to me\n"
+											 + "Six geese a-laying,\n"
+											 + "Five golden rings,\n"
+											 + "Four calling birds,\n"
+											 + "Three French hens,\n"
+											 + "Two turtle doves,\n"
+											 + "And a partridge in a pear tree.\n" );
+				break;
+				case 7 : System.out.println( "On the seventh day of Christmas,\n"
+											 + "my true love sent to me\n"
+											 + "Seven swans a-swimming,\n"
+											 + "Six geese a-laying,\n"
+											 + "Five golden rings,\n"
+											 + "Four calling birds,\n"
+											 + "Three French hens,\n"
+											 + "Two turtle doves,\n"
+											 + "And a partridge in a pear tree.\n" );
+				break;
+				case 8 : System.out.println( "On the eighth day of Christmas,\n"
+											 + "my true love sent to me\n"
+											 + "Eight maids a-milking,\n"
+											 + "Seven swans a-swimming,\n"
+											 + "Six geese a-laying,\n"
+											 + "Five golden rings,\n"
+											 + "Four calling birds,\n"
+											 + "Three French hens,\n"
+											 + "Two turtle doves,\n"
+											 + "And a partridge in a pear tree.\n" );
+				break;
+				case 9 : System.out.println( "On the ninth day of Christmas,\n"
+											 + "my true love sent to me\n"
+											 + "Nine ladies dancing,\n"
+											 + "Eight maids a-milking,\n"
+											 + "Seven swans a-swimming,\n"
+											 + "Six geese a-laying,\n"
+											 + "Five golden rings,\n"
+											 + "Four calling birds,\n"
+											 + "Three French hens,\n"
+											 + "Two turtle doves,\n"
+											 + "And a partridge in a pear tree.\n" );
+				break;
+				case 10 : System.out.println( "On the tenth day of Christmas,\n"
+											  + "my true love sent to me\n"
+											  + "Ten lords a-leaping,\n"
+											  + "Nine ladies dancing,\n"
+											  + "Eight maids a-milking,\n"
+											  + "Seven swans a-swimming,\n"
+											  + "Six geese a-laying,\n"
+											  + "Five golden rings,\n"
+											  + "Four calling birds,\n"
+											  + "Three French hens,\n"
+											  + "Two turtle doves,\n"
+											  + "And a partridge in a pear tree.\n" );
+				break;
+				case 11 : System.out.println( "On the eleventh day of Christmas,\n"
+											  + "my true love sent to me\n"           
+											  + "Eleven pipers piping,\n"             
+											  + "Ten lords a-leaping,\n"              
+											  + "Nine ladies dancing,\n"              
+											  + "Eight maids a-milking,\n"            
+											  + "Seven swans a-swimming,\n"           
+											  + "Six geese a-laying,\n"               
+											  + "Five golden rings,\n"                
+											  + "Four calling birds,\n"               
+											  + "Three French hens,\n"                
+											  + "Two turtle doves,\n"                 
+											  + "And a partridge in a pear tree.\n" );
+
+				break;
+				case 12 : System.out.println( "On the twelfth day of Christmas,\n"
+											  + "my true love sent to me\n"           
+											  + "Twelve drummers drumming,\n"         
+											  + "Eleven pipers piping,\n"             
+											  + "Ten lords a-leaping,\n"              
+											  + "Nine ladies dancing,\n"              
+											  + "Eight maids a-milking,\n"            
+											  + "Seven swans a-swimming,\n"           
+											  + "Six geese a-laying,\n"               
+											  + "Five golden rings,\n"                
+											  + "Four calling birds,\n"               
+											  + "Three French hens,\n"                
+											  + "Two turtle doves,\n"                 
+											  + "And a partridge in a pear tree!\n" );
+
+				break;
+			}
+		}
+	}
+}
